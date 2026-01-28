1038acace6eedb8690ab21de6c58d107b8d32de7
FrancescoCaporale <frenk.caporale@gmail.com>
2026-01-28 16:22:18 +0100

    fix navbar

--- src/layouts/DefaultLayout.jsx
+++ src/layouts/DefaultLayout.jsx
@@ -1,9 +1,10 @@
 import { Outlet } from "react-router-dom";
-import Navbar from "../components/Navbar"
+import Navbar from "../components/Navbar";
 
 export default function DefaultLayout() {
     return (
         <div>
+            <Navbar />
             <h1>Layout Principale</h1>
             <Outlet />
         </div>
